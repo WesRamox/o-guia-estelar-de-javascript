a1bdde1c81b5f758b9eaeb58ba3c8786a747d753
Wesley Ramos <wesleyramox258@gmail.com>
2023-01-06 22:03:02 -0300

    Manipulando dados

---
+++ Manipulando dados/Manipulando 1.js
@@ -0,0 +1,47 @@
+/* 
+
+    Prototype
+
+    * prototype-based language
+    * prototype chain
+    * __proto__
+    
+*/
+
+/* 
+
+    Type conversion (typecasting) vs Type coersion
+
+    * Alterando de um tipo de dado para outro tipo
+
+*/
+
+// console.log(Number('9') + 5)
+
+// Manipulando Strings e Números
+
+// Transformar String em Número e Número em string
+
+// let string = "Wesley"
+// Number(string)
+
+// let number = 172.3293232
+// String(number)
+
+// console.log(string.length)
+// console.log("R$ " +number.toFixed(2).replace(".", ","))
+
+// let phrase = "Eu sou o melhor programador do Brasil"
+// let myArray = phrase.split(" ")
+// let phraseWithUnderscore = myArray.join(" ")
+// console.log(phraseWithUnderscore.toUpperCase())
+
+// let phrase = "Eu quero viver o Amor!"
+// includes é Case Sensitive
+// console.log(phrase.includes("Amor"))
+
+// let myArray = new Array('a', 'b', 'c')
+// console.log(myArray)
+
+// let word = "manipulação"
+// console.log(Array.from(word))
