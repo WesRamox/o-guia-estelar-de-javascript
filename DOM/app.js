de6190f7329415540b6c2510d1ce01f80dd8d72c
Wesley Ramos <wesleyramox258@gmail.com>
2023-01-09 20:49:35 -0300

    DOM

---
+++ DOM/app.js
@@ -0,0 +1,7 @@
+// const element = document.querySelector('h1')
+
+// element.textContent += " Olá, devs"
+
+// element.innerHTML = "Olá Wesley! <small>!!!</small>"
+
+const element = document.querySelector('h1')
